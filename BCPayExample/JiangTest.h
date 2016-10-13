9f58b73d22e9e8ce11e71e0818218787591657c6
降瑞雪 <cyjiangruixue@163.com>
2016-10-13 22:56:29 +0800

    add JiangTest.h .m

---
+++ BCPayExample/JiangTest.h
@@ -0,0 +1,13 @@
+//
+//  JiangTest.h
+//  BCPay
+//
+//  Created by 降瑞雪 on 2016/10/13.
+//  Copyright © 2016年 BeeCloud. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JiangTest : NSObject
+
+@end
